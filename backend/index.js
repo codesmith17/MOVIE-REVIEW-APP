b1a45d29d9e7197bb1832254a294c810ca3ebd99
codesmith17 <kanchanlatakrishna@gmail.com>
2024-06-02 12:14:28 +0530

    changes minor

--- backend/index.js
+++ backend/index.js
@@ -3,24 +3,29 @@ const bodyParser = require("body-parser");
 const cors = require('cors');
 const mongoose = require("mongoose");
 const app = express();
-const process = require("process")
+const process = require("process");
+
 app.use(express.json());
 app.use(cors({
-    origin: 'https://movie-review-app-inky.vercel.app/', // Replace with your React app's URL
+    origin: 'https://movie-review-app-inky.vercel.app', // Replace with your React app's URL
     credentials: true
 }));
+
 const port = process.env.PORT || 3000;
 const authRoutes = require("./routes/Auth.route");
 const reviewRoutes = require("./routes/Review.route");
 const movieRoutes = require("./routes/Movie.route");
-const commentRoutes = require("./routes/Comment.route")
+const commentRoutes = require("./routes/Comment.route");
+
 mongoose.connect("mongodb+srv://krishna170902:44AueKgqHr2eDL8o@clusteracademind.ub2btq6.mongodb.net/movies-app?retryWrites=true&w=majority&appName=ClusterAcademind")
     .then(result => {
         console.log("CONNECTED");
         app.listen(port, () => console.log(`Dolphin app listening on port ${port}!`));
     })
+    .catch(err => console.log(err));
+
 app.use(bodyParser.urlencoded({ extended: false }));
-// const router = express.Router();
+
 app.use("/api/auth", authRoutes);
 app.use("/api/review", reviewRoutes);
 app.use("/api/movie", movieRoutes);
